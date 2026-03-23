f33815159b8b32918f9fe78844863c923e42da6c
Omkar Balasaheb Deshmukh <167670795+OmkarDeshmukh09@users.noreply.github.com>
2026-03-23 23:46:05 +0530

    Update program17_1.py

--- Assignment_17/program17_1.py
+++ Assignment_17/program17_1.py
@@ -6,6 +6,7 @@ def main():
     Data2 = int(input("Enter a 2nd Number : "))
 
     Arithmetic.Addition(Data1, Data2)
+    Arithmetic.Addition(Data1, Data2)
     Arithmetic.Substraction(Data1, Data2)
     Arithmetic.Multiplication(Data1, Data2)
     Arithmetic.Division(Data1, Data2)
